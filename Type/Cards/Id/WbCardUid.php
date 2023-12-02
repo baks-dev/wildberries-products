251124da9a3057fbdd845d9dc3b54dcc4fcf1dde
kingstar <admin@baks.dev>
2023-12-02 12:29:53 +0300

    Baks Development

--- Type/Cards/Id/WbCardUid.php
+++ Type/Cards/Id/WbCardUid.php
@@ -39,7 +39,7 @@ final class WbCardUid extends Uid
 
 
     public function __construct(
-        AbstractUid|string|null $value = null,
+        AbstractUid|self|string|null $value = null,
         mixed $attr = null,
         mixed $option = null,
         mixed $property = null,
