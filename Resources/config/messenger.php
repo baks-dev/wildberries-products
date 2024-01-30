6df6678b5c616382ee1750ee80515853373a1881
kingstar <admin@baks.dev>
2024-01-30 22:43:13 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -33,11 +33,17 @@ return static function(FrameworkConfig $framework) {
         ->transport('wildberries-products')
         ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
         ->options(['queue_name' => 'wildberries-products'])
+        ->failureTransport('failed-wildberries-products')
         ->retryStrategy()
         ->maxRetries(3)
         ->delay(1000)
         ->maxDelay(0)
         ->multiplier(3) // увеличиваем задержку перед каждой повторной попыткой
-        ->service(null);
+        ->service(null)
+
+    ;
+
+    $messenger->transport('failed-wildberries-products')
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%');
 
 };
